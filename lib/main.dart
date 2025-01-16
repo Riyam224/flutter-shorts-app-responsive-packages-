e6439ae289091ae8b2d63f02cef792138c38dc03
Riyam224 <cs50riyam221@gmail.com>
2025-01-16 03:11:27 +0300

    responsive builder package

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,78 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 void main() {
-  runApp(const MyRespo());
+  runApp(const MyApp());
 }
 
-class MyRespo extends StatelessWidget {
-  const MyRespo({super.key});
+// todo screenutil package
+// class MyRespo extends StatelessWidget {
+//   const MyRespo({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return ScreenUtilInit(
+//         designSize: const Size(360, 690),
+//         builder: (context, child) {
+//           return const MaterialApp(
+//               debugShowCheckedModeBanner: false, home: HomeApp());
+//         });
+//   }
+// }
+// // todo
+
+// class HomeApp extends StatelessWidget {
+//   const HomeApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.black,
+//       body: Center(
+//         child: Container(
+//             width: 200.w,
+//             height: 100.h,
+//             color: Colors.blue,
+//             child: Center(
+//                 child: Text(
+//               'screenutil ',
+//               style: TextStyle(fontSize: 20.sp, color: Colors.white),
+//             ))),
+//       ),
+//     );
+//   }
+// }
+
+// todo responsive builder package
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-        designSize: const Size(360, 690),
-        builder: (context, child) {
-          return const MaterialApp(
-              debugShowCheckedModeBanner: false, home: HomeApp());
-        });
+    return MaterialApp(home: HomeScreen());
   }
 }
-// todo
 
-class HomeApp extends StatelessWidget {
-  const HomeApp({super.key});
-
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Container(
-            width: 200.w,
-            height: 100.h,
-            color: Colors.blue,
-            child: Center(
-                child: Text(
-              'screenutil ',
-              style: TextStyle(fontSize: 20.sp, color: Colors.white),
-            ))),
+      appBar: AppBar(
+        title: const Text('Responsive App with ResponsiveBuilder'),
+      ),
+      body: ResponsiveBuilder(
+        builder: (context, sizingInformation) {
+          if (sizingInformation.deviceScreenType == DeviceScreenType.desktop) {
+            return const Text('Desktop Layout');
+          } else if (sizingInformation.deviceScreenType ==
+              DeviceScreenType.tablet) {
+            return const Text('Tablet Layout');
+          } else {
+            return const Text('Mobile Layout');
+          }
+        },
       ),
     );
   }
